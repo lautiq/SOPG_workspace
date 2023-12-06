5f8c8000099d59661c9c7a5ecd73be69c0f387f3
lautiq <110248182+lautiq@users.noreply.github.com>
2023-12-06 10:10:30 -0300

    Update reader.c

--- TP-1-SOPG/reader.c
+++ TP-1-SOPG/reader.c
@@ -16,31 +16,41 @@ void signalHandler(int signal);
 int main(void)
 {
 	uint8_t inputBuffer[BUFFER_SIZE];
-	uint32_t bytesRead;
-	int32_t returnCode, fd;
+	int32_t bytesRead;
+	int32_t bytesWrote;
+	int32_t returnCode, fd, fd_log, fd_sign;
+	
     FILE *textLogFile, *signalLogFile;
-    
+
+    /*Create log.txt */
+	if ((fd_log = open("Log.txt", O_CREAT | O_APPEND | O_WRONLY, 0666)) == -1)
+	{ 
+		printf("Error creating log file: %d\n", returnCode);
+		exit(1);
+	}
+    /*Create Sign.txt */
+	if( (fd_sign = open("Sign.txt", O_CREAT | O_APPEND | O_WRONLY, 0666)) == -1 )
+	{
+		printf("Error creating sign file: %d\n", returnCode);
+		exit(1);
+	}
+
+	
     /* Create named fifo. No action if already exists */
-    if ( (returnCode = mknod(FIFO_NAME, S_IFIFO | 0666, 0) ) != -1  )
-    {
-        printf("Error creating named fifo: %d\n", returnCode);
-        exit(1);
-    }
+	if ((returnCode = mknod(FIFO_NAME, S_IFIFO | 0666, 0)) != -1)
+	{
+	printf("Error creating named fifo: %d\n", returnCode);
+	exit(1);
+	}
     
     /* Open named fifo. Blocks until other process opens it */
 	printf("waiting for writers...\n");
-	if ( (fd = open(FIFO_NAME, O_RDONLY) ) < 0 )
-    {
-        printf("Error opening named fifo file: %d\n", fd);
-        exit(1);
-    }
+	if ((fd = open(FIFO_NAME, O_RDONLY)) < 0)
+	{
+	printf("Error opening named fifo file: %d\n", fd);
+	exit(1);
+	}
     
-    /* Install signal handler for handling signals from console*/
-    signal(SIGINT, signalHandler);
-
-    /* Open log  files for text and signals */
-    textLogFile = fopen("log.txt", "a");
-    signalLogFile = fopen("signal.txt", "a");
 
     /* open syscalls returned without error -> other process attached to named fifo */
 	printf("got a writer\n");
@@ -55,30 +65,48 @@ int main(void)
         }
         else
 		{
-			inputBuffer[bytesRead] = '\0';
-			
-            if(strncmp(inputBuffer, "DATA:", 5) == 0)
-            {
-                //log text to file
-                fprintf(textLogFile, "%s\n", inputBuffer + 5); // skip the "DATA:" prefix
-            } else if (strncmp(inputBuffer, "SIGN:", 5) == 0) 
-            {
-                //log signal to file
-                fprintf(signalLogFile, "%s\n", inputBuffer +5); //skip the "SIGN:" prefix
-            }
-            printf("reader: read %d bytes: \"%s\"\n", bytesRead, inputBuffer);
+			if(inputBuffer[0] == 'D')
+			{
+				inputBuffer[bytesRead] = '\0';
+				fprintf(stdout, "Reader process received %d bytes %s \n", bytesRead, inputBuffer);
+				
+				inputBuffer[bytesRead] = '\n';
+				if((bytesWrote = write(fd_log, inputBuffer, bytesRead+1)) ==-1)
+				{
+					perror("write");
+				}
+				else
+				{
+					fprintf(stdout,"Log.txt: write %d bytes \n", bytesWrote);
+				}
+			}
+
+			if(inputBuffer[0] == 'S')
+			{
+				inputBuffer[bytesRead] = '\0';
+				fprintf(stdout, "Reader process received %d bytes %s \n", bytesRead, inputBuffer);
+
+				inputBuffer[bytesRead] = '\n';
+				if((bytesWrote = write(fd_sign, inputBuffer, bytesRead+1)) ==-1)
+				{
+					perror("write");
+				}
+				else
+				{
+					fprintf(stdout,"Sign.txt: write %d bytes \n", bytesWrote);
+				}
+			}
 		}
 	}
+			
+			
 	while (bytesRead > 0);
 
     //Close log files
-    fclose(textLogFile);
-    fclose(signalLogFile);
+	close(fd_log);
+	close(fd_sign);
+	close(fd);
 
 	return 0;
 }
 
-void signalHandler(int signal) {
-    printf("reader: Received signal %d\n", signal);
-    exit(0);
-}
